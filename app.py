b7835b680016b77fa8a85b6cb8db623bf5a4f556
maainul <mainul080@yahoo.com>
2025-01-04 21:27:15 +0600

    add new

--- app.py
+++ app.py
@@ -10,5 +10,9 @@ def hello_world():
 def hello_world():
     return 'Hi, Mainul!'
 
+@app.route('/show')
+def hello_world():
+    return 'Show, Mainul!'
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
