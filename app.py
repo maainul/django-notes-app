9a229d1378c93671675b647ffdcc521ac965f210
maainul <mainul080@yahoo.com>
2025-01-05 01:27:43 +0600

    update master

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, Mainul!'
+@app.route('/master')
+def hello_master():
+    return 'Hello, Mainul Master!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
